c1b5c804dc8317b4cfa0f5cec8ef9a316f542162
Tasin Ishmam <tasinishmam@gmail.com>
2020-12-19 11:34:07 +0600

    Add Payments

---
+++ database/models/Payments.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+
+// create booking Schema and model
+const PaymentSchema = new Schema({
+
+    customerId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Customer'
+    },
+
+    BookingId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Booking'
+    },
+
+    amount: {
+        type: Number,
+        required: true
+    },
+
+    paymentTime: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Payment = mongoose.model('Payment', PaymentSchema);
+
+module.exports = Payment;
